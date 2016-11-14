bc6051b84c987b3c67aade9636411a55a301bafc
Eddie Martin <eddie@eddies-air.resource.campus.njit.edu>
2016-11-14 12:48:20 -0500

    start js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 'use strict'
 
+init();
+render();
+animate();
+
+//create scene
+function init(){
+
+   var scene = new THREE.Scene();
+   var camera = new THREE.PerspectiveCamera(75, window.innerWidth / 
+         Window.innerHeight, 0.1, 1000);
+   
+   camera.position.z = 5;
+   
+}
+
+//create renderer
+function render(){
+
+   var renderer = new THREE.WebGLRenderer();
+
+}
+
+//animate object
+function animate(){
+
+}
+
 
